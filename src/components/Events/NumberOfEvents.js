fe816842b087058f9078369a8b0661b2d89117b1
Schnecke <eiweala@live.com>
2020-09-06 13:25:05 +0800

    completed

--- src/components/Events/NumberOfEvents.js
+++ src/components/Events/NumberOfEvents.js
@@ -1,28 +1,35 @@
 import React from "react";
+import { ErrorAlert } from "../Alert";
 
 class NumberOfEvents extends React.Component {
   state = {
     length: 32,
+    message: null,
   };
 
   handleChange = (event) => {
     let { value } = event.target;
     this.setState({ length: +value });
-    if (value !== "") {
+    if (value <= 0) {
+      this.setState({ message: "Number should be greater than 0" });
+    } else {
+      this.setState({ message: null });
       this.props.updateNumberOfEvents(+value);
     }
   };
 
   render() {
-    let { length } = this.state;
+    let { length, message } = this.state;
     // console.log(this.state.length);
     return (
       <div className="NumberOfEvents">
+        {message && <ErrorAlert text={message} />}
         Show{" "}
         <input
           className="eventCount"
           value={length}
           name="eventCount"
+          type="number"
           onChange={this.handleChange}
         />{" "}
         Events
